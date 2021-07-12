93825ee86093895900ecc99fde6f2916b8d11f59
Mohanad Abugharbia <mgharbieh.01@gmail.com>
2021-07-12 14:28:52 +0200

    Reworked how X and O are drawn

--- client/grid.py
+++ client/grid.py
@@ -1,9 +1,6 @@
 import pygame
 import os
 
-letterX = pygame.image.load(r'res/letterX.BMP')
-letterO = pygame.image.load(r'res/letterO.BMP')
-
 class Grid:
 	def __init__(self):
 		self.grid_lines = [	((0, 225), (675, 225)), #first horizontal line
@@ -13,16 +10,20 @@ class Grid:
 		self.grid = [[0 for x in range(3)] for y in range(3)]
 
 	def draw(self, surface):
+		red_color = (255, 0, 0)
 		for line in self.grid_lines:
 			pygame.draw.line(surface, (200, 200, 200), line[0], line[1], 2)
 
 		for y in range(len(self.grid)):
 			for x in range(len(self.grid[y])):
 				if self.get_cell_value(x, y) == "X":
-					surface.blit(letterX, (x*225, y*225))
+					#surface.blit(letterX, (x*225, y*225))
+					pygame.draw.line(surface, red_color, (x*225, y*225), (x*225 + 225, y*225 + 225), 2)
+					pygame.draw.line(surface, red_color, (x*225 + 225, y*225), (x*225, y*225 + 225), 2)
 
 				elif self.get_cell_value(x, y) == "O":
-					surface.blit(letterO, (x*225, y*225))
+					#surface.blit(letterO, (x*225, y*225))
+					pygame.draw.circle(surface, red_color, (x*225 + 112, y*225 + 112), 112, 2)
 
 	def get_cell_value(self, x, y):
 		return self.grid[y][x]
