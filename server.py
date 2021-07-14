f26d184d3717f08b677f47ab66db4c2e1d3432b0
Mohanad Abugharbia <mgharbieh.01@gmail.com>
2021-07-14 18:52:30 +0200

    renamed a few variable to make them more understandable

--- server.py
+++ server.py
@@ -39,11 +39,6 @@ def check_winning_rules():
 	else:
 		return False
 
-def create_thread(target):
-	thread = threading.Thread(target = target)
-	thread.daemon = True
-	thread.start()
-
 
 host = '127.0.0.1'
 port = 5555
@@ -54,12 +49,12 @@ sock.listen(2)
 
 print("Game is ready to be connected to")
 
-def game_start(conn1, conn2):
-	p1_info = '{}'.format("True").encode()
-	p2_info = '{}'.format("False").encode()
+def game_initialistaion(player1, player2):
+	p1_info = 'True'.encode()
+	p2_info = 'False'.encode()
 
-	conn1.send(p1_info)
-	conn2.send(p2_info)
+	player1.send(p1_info)
+	player2.send(p2_info)
 
 def write_onto_the_game_board(x, y, player):
 	if game_board[x][y] == " ":
@@ -67,57 +62,62 @@ def write_onto_the_game_board(x, y, player):
 		return True
 
 
-def send_data_AND_await_response(connection, x, y, player):
-	command = f"{x}-{y}-True-{player}-keepplaying".encode() # x, y, True, yourturn, player
-	connection.send(command)
-	return connection.recv(1024).decode()
+def send_data_AND_wait_for_response(player, x, y, turn):
+	message = f"{x}-{y}-True-{turn}-keepplaying".encode()
+	player.send(message)
+	return player.recv(1024).decode()
 
 
-def receive_data(conn1, conn2):
-	data = conn1.recv(1024).decode()
+def game_start(player1, player2):
+	data = player1.recv(1024).decode()
 	tiles_taken = 0
 	while True:
 		data = data.split('-') # x, y, player
 		x = int(data[0])
 		y = int(data[1])
-		player = str(data[2])
+		turn = str(data[2])
 
-		write_onto_the_game_board(int(data[0]), int(data[1]), player)
+		write_onto_the_game_board(x, y, turn)
 		tiles_taken += 1
 		print(game_board)
 		ruling = check_winning_rules()
 		if ruling == 'X' or ruling == 'O':
 				print(ruling)
-				command = f"{x}-{y}-True-{player}-gameover".encode()
-				conn1.send(command)
-				conn2.send(command)
+				message = f"{x}-{y}-True-{turn}-gameover".encode()
+				player1.send(message)
+				player2.send(message)
 				break
 		if tiles_taken != 9:
-			if player == "X":
-				data = send_data_AND_await_response(conn2, x, y, player)
+			if turn == "X":
+				data = send_data_AND_wait_for_response(player2, x, y, turn)
 			else:
-				data = send_data_AND_await_response(conn1, x, y, player)
+				data = send_data_AND_wait_for_response(player1, x, y, turn)
 		elif ruling == False:
-			command = f"{x}-{y}-True-{player}-draw".encode()
-			conn1.send(command)
-			conn2.send(command)
+			message = f"{x}-{y}-True-{turn}-draw".encode()
+			player1.send(message)
+			player2.send(message)
 			print("Draw")
 			break
 		
 		
-	conn1.close()
-	conn2.close()
+	player1.close()
+	player2.close()
 	print("Clients disconnected")
 
 def waiting_for_connection():
-	conn1, addr1 = sock.accept()
-	print(f"client is connected {addr1[0]} : {addr1[1]}")
-	conn2, addr2 = sock.accept()
-	print(f"client is connected {addr2[0]} : {addr2[1]}")
+	player1, addr1 = sock.accept()
+	print(f"Player1 is connected {addr1[0]} : {addr1[1]}")
+	player2, addr2 = sock.accept()
+	print(f"Player2 is connected {addr2[0]} : {addr2[1]}")
 	connection_established = True
-	game_start(conn1, conn2)
-	receive_data(conn1, conn2)
+	game_initialistaion(player1, player2)
+	game_start(player1, player2)
 
+
+def create_thread(target):
+	thread = threading.Thread(target = target)
+	thread.daemon = True
+	thread.start()
 
 create_thread(waiting_for_connection)
 
